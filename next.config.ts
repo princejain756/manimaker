581c90cce3a4ee98496429cc19cb7b3fd5c92029
princejain756 <princejain756@gmail.com>
2025-08-12 05:54:43 +0530

    push

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['sharp']
+  },
+  images: {
+    domains: ['maninfini.com', 'ai.maninfini.com']
+  },
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
